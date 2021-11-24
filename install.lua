030582982085aa20838a5d502133537ed7d2b1b2
Majestad278 <j.mag.carrion@gmail.com>
2021-11-24 21:12:29 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,5 +1,6 @@
 local GetFileContents = require('GetFileContents')
 local FilePutContents = require('FilePutContents')
+local cpu = require('computer')
 local ui = require('UI')
 local split = require('split')
 local serialization = require('serialization')
@@ -9,13 +10,16 @@ local fs = require('filesystem')
 local component = require('component')
 local gpu = component.gpu
 local modem = component.modem
-local running = true
 term.clear()
 
+do
+    cpu.getDeviceInfo()
+end
+
 function isquarry() -- Function for install main server quarry.
-    -- body
+    
 end
 
 function ibquarry() -- Function for install robot quarry service.
-    -- body
+    
 end
